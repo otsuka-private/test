e8513c629f3d5e3ae436f0766f6bf0735e1289a5
大塚遥輝 <ootsukaharuki@dazhongyaohuis-MacBook-Pro.local>
2021-02-13 17:03:15 +0900

    2/3

--- assets/php/accept_form.php
+++ assets/php/accept_form.php
@@ -3,11 +3,25 @@
   <div class="card">
     <div class="card-content">
       <?php
-    $filePath = '../texts/hello.txt';
-    if (file_exists($filePath)) {
-        echo 'file exists!';
-    }
-    ?>
+      $uploadedFile = $_FILES['user-input']['tmp_name'];
+      // 一時保存されたファイル
+      if (is_uploaded_file($uploadedFile)) {
+        if (!file_exists('uploaded_images')) {
+          mkdir('./uploaded_images');
+        }
+        // 保存先予定のフォルダがなければ作成
+        $filePath = './uploaded_images/'.basename($_FILES['user-input']['name']);
+        // 保存先のフォルダへのパスを指定
+        if (move_uploaded_file($uploadedFile, $filePath)) {
+          echo "uploaded!";
+          // 一時保存されたファイルを目的の保存先パスへ移動（結果的に望むように保存された）
+        } else {
+          echo "not uploaded!";
+        }
+      } else {
+        echo "choose file again";
+      }
+      ?>
     </div>
   </div>
 </div>
--- assets/php/index.php
+++ assets/php/index.php
@@ -6,11 +6,20 @@ date_default_timezone_set('Japan');
   <div class="col s12">
     <div class="card">
       <div class="card-content">
-        <form action="./accept_form.php" method="post">
-          <div class="input-field">
+        <form action="./accept_form.php" method="post" enctype="multipart/form-data">
+          <!-- <div class="input-field">
             <p>入力してください。</p>
             <input id="address-number" type="text" class="validate" name="user-input">
-            <!-- <label for="address-number">郵便番号</label> -->
+            <label for="address-number">郵便番号</label>
+          </div> -->
+          <div class="file-field input-field">
+            <div class="btn">
+              <span>Select File</span>
+              <input type="file" name="user-input">
+            </div>
+            <div class="file-path-wrapper">
+              <input class="file-path validate" type="text">
+            </div>
           </div>
           <button class="btn waves-effect waves-light" type="submit">GO <i class="fas fa-paper-plane"></i></button>
         </form>
